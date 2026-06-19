2a9d5d565f03192bd04c4be4ccf97ccdee2fdfac
Learning <snehadani635@gmail.com>
2026-06-19 20:48:28 +0530

    add

--- Arrays/Array3.cpp
+++ Arrays/Array3.cpp
@@ -16,3 +16,12 @@ int main(){
     }
 }
 
+bool find(int arr[], int size, int key){
+    for(int i=0; i<size; i++){
+        if(arr[i]==key){
+            return true;
+        }
+
+        return false;
+    }
+}
