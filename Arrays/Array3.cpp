a8dde1807c37667bf4b8ed9ddee2c36a6c90e909
Learning <snehadani635@gmail.com>
2026-06-19 20:52:29 +0530

    add

--- Arrays/Array3.cpp
+++ Arrays/Array3.cpp
@@ -2,6 +2,15 @@
 #include <iostream>
 using namespace std;
 
+bool find(int arr[], int size, int key){
+    for(int i=0; i<size; i++){
+        if(arr[i]==key){
+            return true;
+        }
+    }
+    return false;
+}
+
 int main(){
     int arr[5] = {1,2,5,9,3};
     int size = 5;
@@ -16,12 +25,3 @@ int main(){
     }
 }
 
-bool find(int arr[], int size, int key){
-    for(int i=0; i<size; i++){
-        if(arr[i]==key){
-            return true;
-        }
-
-        return false;
-    }
-}
