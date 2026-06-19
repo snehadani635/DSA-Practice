4336f80b159c52ddbb25bb22f9b41b797abaa76a
Learning <snehadani635@gmail.com>
2026-06-19 20:45:21 +0530

    add

--- Arrays/Array3.cpp
+++ Arrays/Array3.cpp
@@ -1,2 +1,18 @@
 //Linear Search in array
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[5] = {1,2,5,9,3};
+    int size = 5;
+    cout<<"Enter the key to find: ";
+    int key;
+    cin>>key;
+    if(find(arr,size,key)){
+        cout<<"Found"<<endl;
+    }
+    else{
+        cout<<"Not Found"<<endl;
+    }
+}
 
