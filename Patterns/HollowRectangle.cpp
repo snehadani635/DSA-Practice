9ebd57be32ab903d10248309a0c900c53b3a6d87
Learning <snehadani635@gmail.com>
2026-06-20 22:28:27 +0530

    add

--- Patterns/HollowRectangle.cpp
+++ Patterns/HollowRectangle.cpp
@@ -2,5 +2,25 @@
 using namespace std;
 
 int main(){
-    
+    int rowCount, colCount;
+    cout<<"Enter number of rows:";
+    cin>>rowCount;
+    cout<<"Enter number of columns:";
+    cin>>colCount;
+
+    for(int row=0; row<rowCount; row++){
+        if(row==0 || row==rowCount-1){
+            for(int col=0; col<colCount; col++){
+                cout<<"* ";
+            }
+        }
+        else{
+            cout<<"* ";
+            for(int i=0; i<colCount-2; i++){
+                cout<<"  ";
+            }
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
 }
