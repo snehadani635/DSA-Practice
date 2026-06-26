f700381568ef1992533f1cab39e1c74834585282
Learning <snehadani635@gmail.com>
2026-06-26 16:31:16 +0530

    add

---
+++ Arrays/Array_MajorityElement.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+
+        for(int i = 0; i < nums.size(); i++) {
+
+            int count = 0;
+
+            for(int j = 0; j < nums.size(); j++) {
+
+                if(nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+
+            if(count > nums.size() / 2) {
+                return nums[i];
+            }
+        }
+
+        return -1;
+    }
+};
+
+int main() {
+
+    Solution obj;
+
+    vector<int> nums = {2, 2, 1, 1, 1, 2, 2};
+
+    cout << "Majority Element: " << obj.majorityElement(nums);
+
+    return 0;
+}
